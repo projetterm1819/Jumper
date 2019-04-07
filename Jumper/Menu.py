3559721b208032337fc38ac2320247df7d9d42df
silolis <48384046+siloli@users.noreply.github.com>
2019-04-07 14:18:42 +0200

    Create Menu

---
+++ Jumper/Menu.py
@@ -0,0 +1,30 @@
+from tkinter import * 
+import subprocess as sp
+
+def launchGame():
+	sp.Popen(("python","projet.py")) 
+	exit()
+
+
+ScreenMenu = Tk()
+ScreenMenu.title('Jumper')
+ScreenMenu.geometry("1366x741+0+27")
+w=1366
+h=741
+ScreenMenu.resizable(width=False,height=False)
+image = PhotoImage(file='IMAGES/MenuBackground.png', master=ScreenMenu)
+Background = Canvas(ScreenMenu, width=w, height=h)
+Background.pack()
+
+Background.create_image((w//2, h//2), image=image)
+
+ScreenMenu.bind('<Escape>',exit)
+
+with open("Files/Score","r") as file:
+	score = file.read()
+Background.create_text(1366/2,741/2,text="Score : "+score)
+ButtonPicture= PhotoImage(file='IMAGES/JumperButton.png')
+Bouton = Button(image=ButtonPicture, command = launchGame)
+Bouton.place(bordermode=OUTSIDE, height=76, width=512,x=423,y=226)
+ScreenMenu.mainloop()
+
